5f9cac7fe7800fee0d34ac24fc5addc3096a623d
NLIVEKOREA <won4646@naver.com>
2026-06-10 15:07:15 +0900

    SEO: allow crawling of /privacy and /terms

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: "*", allow: "/", disallow: ["/privacy", "/terms"] },
+      { userAgent: "*", allow: "/" },
       { userAgent: "Yeti", allow: "/" },
       { userAgent: "Googlebot", allow: "/" },
       { userAgent: "Bingbot", allow: "/" },
